5c894bd502ec1df4eceabe9f10255f1d441796ec
Jim Tyhurst <jim@tyhurst.com>
2016-12-14 21:26:51 -0800

    Added test cases for bigq.

--- tests/testthat/test-gmp.R
+++ tests/testthat/test-gmp.R
@@ -29,3 +29,18 @@ test_that("Okay to convert individual operands", {
   expect_equal(sub.bigz(as.bigz("9876543210987654321"), as.bigz("9876543210987654320")), 1)
 })
 
+test_that("Accuracy is preserved for large floats", {
+  # This works fine with 15 digits:
+  expect_equal(999999999999999.0 - 999999999999998.0, 1)
+})
+
+test_that("Accuracy fails for floats that are too large", {
+  # Surprise! With 16 digits, result is 2, rather than 1:
+  expect_equal(9999999999999999.0 - 9999999999999998.0, 2)
+})
+
+test_that("gmp preserves accuracy for floats that are 'too large'", {
+  # This works fine with 16 digits, but you have to represent
+  # the number with integer numerator and denominator:
+  expect_equal(sub.bigq(as.bigq("9999999999999994", 3), as.bigq("9999999999999991", 3)), 1)
+})
